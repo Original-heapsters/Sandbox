211c041d717e45dc4911f0c1d95cb27398ab94e8
Oliver Kim <oliver94s@yahoo.com>
2016-02-06 20:52:00 -0800

    Check withs with txt files. There is a test txt file called test_url.txt

--- check.py
+++ check.py
@@ -1,13 +1,31 @@
 import os
 import json
+import sys
 from pprint import pprint
+from pyshorteners import Shortener
 
-url = "http://www.clarifai.com/img/metro-north.jpg"
-command = 'curl -H "Authorization: Bearer b3A9PCVEzVkAijC1CC0qEUPNKcS9GE" --data-urlencode "url=http://www.clarifai.com/img/metro-north.jpg"  https://api.clarifai.com/v1/tag/'
-command = command + "| python -mjson.tool > test.json"
-os.system(command)
+def main(argv):
+    if len(argv) > 1:
+        url = argv
+        if len(argv) > 20:
+            shortener = Shortener('TinyurlShortener')
+            url = shortener.short(url)
+    else:
+        print("ERROR")
+        url = "http://www.clarifai.com/img/metro-north.jpg"
 
-with open('test.json') as test:
-    data = json.load(test)
+    command = 'curl -H "Authorization: Bearer b3A9PCVEzVkAijC1CC0qEUPNKcS9GE" --data-urlencode "url="'
 
-pprint(data['results'][0]['result']['tag']['classes'])
+    command = command + url + " https://api.clarifai.com/v1/tag/"
+    command = command + "| python -mjson.tool > test.json"
+    os.system(command)
+
+    with open('test.json') as test:
+        data = json.load(test)
+
+    pprint(data['results'][0]['result']['tag']['classes']) 
+
+if __name__ == '__main__':
+    with open('test_url.txt', 'r') as link:
+        for x in link:
+            main(x)
