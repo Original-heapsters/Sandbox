0058510545227908c641b3d5f2aaafd912c30265
Russell Tan <sell_nat@yahoo.com>
2016-02-07 06:17:44 -0800

    Added comments and combined loc_fb with fb_analysis

--- loc_fb.py
+++ loc_fb.py
@@ -21,18 +21,15 @@ class loc_fab:
         feed = urllib2.urlopen("https://graph.facebook.com/v2.4/me?fields=location&access_token="+str(self.token)).read()
         parsed = json.loads(feed)
         json_dump = json.dumps(parsed, indent=4,sort_keys=True)
-        loc_ids = self.extract_locName(json_dump)
+        self.extract_locName(json_dump)
 
     def extract_locName(self, json_obj):
         name = list()
-        count = 0
         for line in json_obj.splitlines():
             if "name" in line:
-             count += 1
              colon_index = line.find(":")
              extracted_locName = line[colon_index+2:].replace("\"","")
-             name.append(extracted_locName)
-        return name
+             self.names.append(extracted_locName)
 
     def write_location_file(self,filename):
         with open(filename,'wb') as fout:
@@ -40,7 +37,7 @@ class loc_fab:
                 fout.write(name+"\r\n")	
 
 if __name__ == "__main__":
-    fb_obj = loc_fab(token="CAACEdEose0cBAAtGRDVs7rrm8plggUrzyoQYHOmX5ZBnn5npT2yhqY4mpalZAdt8HHZCFLcGklsOZBstyQO1yeS14V8T9ZCXhLzZAtDfxLG4BDhiHiTUPowpUDZBmABOOao1piy7736jQ868ok7dDjX3LaIiLl7f181vlqseHZB8ZB0tMCoJJ2w6yG0ZCZA19nSCU2ZBEtXbsyj0IqCDZAe4ZAlm3S",user_id="758447787538137")
+    fb_obj = loc_fab(token="CAACEdEose0cBAHv0pn3hY63Y5GznI8c109PL6zVVCu8TOV9qirQO2QtYSXnxoBZBZAGsJVBi0k1ISqaK2t73ZAcCTkX5ZBT6SVlZCJcsMDFlOKzZCm9iji7BT8KWVfwy2J2hKqrZAP1oUAAZBNINYv7gFZAZCff5HlbwXj6Sr0OZA57xU3IYmEZBlSQbVd0QZA7EeeHLZAo1ja1OYq40KNXAGkSRdc",user_id="758447787538137")
     
     fb_obj.init_fb()
 
